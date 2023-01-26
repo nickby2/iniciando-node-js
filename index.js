2e465aec792f4f4b4d3cec4b9e2a398075e27052
Lucas <monteiro9961@gmail.com>
2023-01-26 13:11:08 -0300

    Criando um projeto em node.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello Word!'));
+
 console.log('olá mundo');
+
+console.log("São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.");
